9590ed6355fe92b5331c250d2f44bd334e158211
Johannes Tegnér <johannes@jitesoft.com>
2019-03-25 13:00:44 +0100

    Webpack config.

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const getEnv = () => {
+  return process.env.NODE_ENV === 'production' ? 'production' : 'development';
+};
+
+console.log(getEnv());
+
+const Path = require('path');
+
+module.exports = {
+  mode: getEnv(),
+  optimization: {
+    minimize: getEnv() === 'production'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  entry: {
+    'index': [
+      Path.join(__dirname, 'src', 'index.js')
+    ]
+  },
+  output: {
+    filename: 'index.js',
+    library: 'sprintf',
+    libraryTarget: 'umd'
+  }
+};
