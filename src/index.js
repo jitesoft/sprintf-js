06b54472ce54bc78d684a5c00768f53346429551
Johannes Tegnér <johannes@jitesoft.com>
2019-03-25 11:53:05 +0100

    Initial code (no build script yet).

---
+++ src/index.js
@@ -0,0 +1,111 @@
+/**
+ * Sprintf - String Print Format.
+ *
+ * Creates a string using the format parameter, if format includes format specifiers (begins with %) the additional
+ * arguments following format are formatted and inserted in the resulting string replacing their respective specifiers.
+ *
+ * Available placeholders:
+ * <pre>
+ *   i: Integer
+ *   o: Octal
+ *   x: Hex (lower case)
+ *   X: Hex (upper case)
+ *   e: Scientific notation (lower case)
+ *   E: Scientific notation (upper case)
+ *   d: Decimal
+ *   f: Float
+ *   a: Hex float (lower case)
+ *   A: Hex float (upper case)
+ *   %c: Char
+ *   %s: String
+ *   %j: Json (object which will be stringified if possible)
+ * </pre>
+ *
+ * @param {String} format
+ * @param {...*} [args]
+ * @return {String}
+ */
+export default function sprintf(format, ...args) {
+  const lastIndex = format.lastIndexOf('%');
+  if (args.length === 0 || lastIndex === -1) {
+    return format;
+  }
+
+  args = args.reverse();
+  const len = format.length;
+  let char = '';
+  let result = '';
+
+  for (let i = 0; i < len; i++) {
+    char = format.charAt(i);
+    if (char === '%') {
+      // Has next?
+      if (i !== len) {
+        // Yes next, check next
+        char = format.charAt(++i);
+        if (char !== '%') { // If %, we just skip it and allow it to add it as percent.
+          char = types[char](args.pop());
+        }
+      }
+    }
+    result += char;
+    if (args.length === 0 || i > lastIndex) {
+      return `${result}${format.substr(i + 1)}`;
+    }
+  }
+
+  return result;
+};
+
+const types = {
+  /* Integer */
+  i: (val) => parseInt(val)?.toString(10) || 'NaN',
+  /* Octal */
+  o: (val) => parseInt(val)?.toString(8) || 'NaN',
+  /* Hex (lower case) */
+  x: (val) => {
+    val = parseInt(val);
+    return isNaN(val) ? 'NaN' : val.toString(16).toLowerCase();
+  },
+  /* Hex (upper case) */
+  X: (val) => {
+    val = parseInt(val);
+    return isNaN(val) ? 'NaN' : val.toString(16).toUpperCase();
+  },
+  /* Scientific notation (lower case) */
+  e: (val) => {
+    val = Number(val);
+    return isNaN(val) ? 'NaN' : val.toExponential().toLowerCase();
+  },
+  /* Scientific notation (upper case) */
+  E: (val) => {
+    val = Number(val);
+    return isNaN(val) ? 'NaN' : val.toExponential().toUpperCase();
+  },
+  /* Decimal */
+  d: (val) => Number(val),
+  /* Float */
+  f: (val) => {
+    val = parseFloat(val);
+    return isNaN(val) ? 'NaN' : val.toString(10);
+  },
+  /* Hex float (lower case) */
+  a: (val) => {
+    val = parseFloat(val);
+    return isNaN(val) ? 'NaN' : val.toString(16).toLowerCase();
+  },
+  /* Hex float (upper case) */
+  A: (val) => {
+    val = parseFloat(val);
+    return isNaN(val) ? 'NaN' : val.toString(16).toUpperCase();
+  },
+  /* Char */
+  c: (val) => {
+    val = String(val);
+    return val.length <= 0 ? '[NULL]' : val[0]
+  },
+  /* String */
+  s: (val) => String(val),
+  /* Json (object which will be stringified if possible) */
+  j: (val) => JSON.stringify(val)
+};
---
+++ test/sprintf.test.js
@@ -0,0 +1,107 @@
+import sprintf from '../src/index';
+
+describe('Tests for sprintf.', () => {
+  describe('Tests for variable substitution.', () => {
+
+    describe('Integer types', () => {
+      test('Int', () => {
+        expect(sprintf('Abc %i EFG', 5)).toEqual('Abc 5 EFG');
+        expect(sprintf('Abc %i %i %i %i EFG', 5, 9, 12, 100)).toEqual('Abc 5 9 12 100 EFG');
+        expect(sprintf('Abc %i %i EFG', 5)).toEqual('Abc 5 %i EFG');
+        expect(sprintf('Abc %i %i %i EFG', 5, 'abc', 10)).toEqual('Abc 5 NaN 10 EFG');
+      });
+
+      test('Int - Hex', () => {
+        expect(sprintf('Abc %x EFG', 5)).toEqual('Abc 5 EFG');
+        expect(sprintf('Abc %x %x %x %x EFG', 5, 9, 12, 100)).toEqual('Abc 5 9 c 64 EFG');
+        expect(sprintf('Abc %x %x EFG', 5)).toEqual('Abc 5 %x EFG');
+        expect(sprintf('Abc %x %x %x EFG', 5, 'abc', 10)).toEqual('Abc 5 NaN a EFG');
+      });
+
+      test('Int - Hex (upper case)', () => {
+        expect(sprintf('Abc %X EFG', 5)).toEqual('Abc 5 EFG');
+        expect(sprintf('Abc %X %X %X %X EFG', 5, 9, 12, 100)).toEqual('Abc 5 9 C 64 EFG');
+        expect(sprintf('Abc %X %X EFG', 5)).toEqual('Abc 5 %X EFG');
+        expect(sprintf('Abc %X %X %X EFG', 5, 'abc', 10)).toEqual('Abc 5 NaN A EFG');
+      });
+
+      test('Int - Octal', () => {
+        expect(sprintf('Abc %o EFG', 5)).toEqual('Abc 5 EFG');
+        expect(sprintf('Abc %o %o %o %o EFG', 5, 9, 12, 100)).toEqual('Abc 5 11 14 144 EFG');
+        expect(sprintf('Abc %o %o EFG', 5)).toEqual('Abc 5 %o EFG');
+        expect(sprintf('Abc %o %o %o EFG', 5, 'abc', 10)).toEqual('Abc 5 NaN 12 EFG');
+      });
+
+      test('Int - Scientific Notation', () => {
+        expect(sprintf('Abc %e EFG', 5)).toEqual('Abc 5e+0 EFG');
+        expect(sprintf('Abc %e %e %e %e EFG', 5, 9, 12, 10000000000)).toEqual('Abc 5e+0 9e+0 1.2e+1 1e+10 EFG');
+        expect(sprintf('Abc %e %e EFG', 5123123123123123123)).toEqual('Abc 5.123123123123123e+18 %e EFG');
+        expect(sprintf('Abc %e %e %e EFG', 5, 'abc', 10)).toEqual('Abc 5e+0 NaN 1e+1 EFG');
+      });
+
+      test('Int - Scientific Notation (upper case)', () => {
+        expect(sprintf('Abc %E EFG', 5)).toEqual('Abc 5E+0 EFG');
+        expect(sprintf('Abc %E %E %E %E EFG', 5, 9, 12, 10000000000)).toEqual('Abc 5E+0 9E+0 1.2E+1 1E+10 EFG');
+        expect(sprintf('Abc %E %E EFG', 5123123123123123123)).toEqual('Abc 5.123123123123123E+18 %E EFG');
+        expect(sprintf('Abc %E %E %E EFG', 5, 'abc', 10)).toEqual('Abc 5E+0 NaN 1E+1 EFG');
+      });
+    });
+
+    describe('Float types', () => {
+      test('Double', () => {
+        expect(sprintf('Abc %d EFG', 5.123)).toEqual('Abc 5.123 EFG');
+        expect(sprintf('Abc %d %d %d %d EFG', 5, 9.123, 12, 1000000.00001)).toEqual('Abc 5 9.123 12 1000000.00001 EFG');
+        expect(sprintf('Abc %d %d EFG', 512312.123)).toEqual('Abc 512312.123 %d EFG');
+        expect(sprintf('Abc %d %d %d EFG', 5.22, 'abc', 10.123)).toEqual('Abc 5.22 NaN 10.123 EFG');
+      });
+
+      test('Float', () => {
+        expect(sprintf('Abc %f EFG', 5.123)).toEqual('Abc 5.123 EFG');
+        expect(sprintf('Abc %f %f %f %f EFG', 5, 9.123, 12, 1000000.00001)).toEqual('Abc 5 9.123 12 1000000.00001 EFG');
+        expect(sprintf('Abc %f %f EFG', 512312.123)).toEqual('Abc 512312.123 %f EFG');
+        expect(sprintf('Abc %f %f %f EFG', 5.22, 'abc', 10.123)).toEqual('Abc 5.22 NaN 10.123 EFG');
+      });
+
+      test('Float - Hex', () => {
+        expect(sprintf('Abc %a EFG', 5.123)).toEqual('Abc 5.1f7ced916872c EFG');
+        expect(sprintf('Abc %a %a %a %a EFG', 5, 9.123, 12, 1000000.00001)).toEqual('Abc 5 9.1f7ced9168728 c f4240.0000a7c58 EFG');
+        expect(sprintf('Abc %a %a EFG', 512312.123)).toEqual('Abc 7d138.1f7ced918 %a EFG');
+        expect(sprintf('Abc %a %a %a EFG', 5.22, 'abc', 10.123)).toEqual('Abc 5.3851eb851eb84 NaN a.1f7ced9168728 EFG');
+      });
+
+      test('Float - Hex (upper case)', () => {
+        expect(sprintf('Abc %A EFG', 5.123)).toEqual('Abc 5.1F7CED916872C EFG');
+        expect(sprintf('Abc %A %A %A %A EFG', 5, 9.123, 12, 1000000.00001)).toEqual('Abc 5 9.1F7CED9168728 C F4240.0000A7C58 EFG');
+        expect(sprintf('Abc %A %A EFG', 512312.123)).toEqual('Abc 7D138.1F7CED918 %A EFG');
+        expect(sprintf('Abc %A %A %A EFG', 5.22, 'abc', 10.123)).toEqual('Abc 5.3851EB851EB84 NaN A.1F7CED9168728 EFG');
+      });
+    });
+
+    describe('String types', () => {
+      test('String', () => {
+        expect(sprintf('Abc %s EFG', 'weeeee')).toEqual('Abc weeeee EFG');
+        expect(sprintf('Abc %s %s %s %s EFG', 'weeeee', 'Hi!', 'cool', 'nice')).toEqual('Abc weeeee Hi! cool nice EFG');
+        expect(sprintf('Abc %s %s EFG', 'testtest')).toEqual('Abc testtest %s EFG');
+        expect(sprintf('Abc %s %s %s EFG', 'something', 'abc', 'yay')).toEqual('Abc something abc yay EFG');
+      });
+
+      test('Char', () => {
+        //[NULL]
+        expect(sprintf('Abc %c EFG', 'A')).toEqual('Abc A EFG');
+        expect(sprintf('Abc %c %c %c %c EFG', 5, '9', 'T', 'g')).toEqual('Abc 5 9 T g EFG');
+        expect(sprintf('Abc %c %c EFG', '5')).toEqual('Abc 5 %c EFG');
+        expect(sprintf('Abc %c %c %c EFG', '5', 'abc', '')).toEqual('Abc 5 a [NULL] EFG');
+
+      });
+
+      test('Json', () => {
+        expect(sprintf('Abc %j EFG', {test: 'A'})).toEqual('Abc {"test":"A"} EFG');
+        expect(sprintf('Abc %j %j %j %j EFG', 5, {val: '9', arr: ['a', 'b'], obj: {t: 1}})).toEqual('Abc 5 {"val":"9","arr":["a","b"],"obj":{"t":1}} %j %j EFG');
+        expect(sprintf('Abc %j %j EFG', '5')).toEqual('Abc "5" %j EFG');
+        expect(sprintf('Abc %j %j %j %j EFG', {}, {a:'b'}, ['a', 45], [])).toEqual('Abc {} {"a":"b"} ["a",45] [] EFG');
+      });
+    });
+  });
+
+});
+
